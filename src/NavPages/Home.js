0364ac9adf4aab87668c823add2943bf72060b78
mayank yadav <mayankyadav@PWMAC191.local>
2025-02-25 19:25:47 +0530

    news added

--- src/NavPages/Home.js
+++ src/NavPages/Home.js
@@ -84,6 +84,7 @@ function Home() {
               <SwiperCustom dataObject={result} />
               {/* <GridSection dataObject={result} /> */}
               <VideoSection/>
+              {/* === video add === */}
               {/* <div className="youtubeshort-container">
                 <div className="sortsHeading">
                   <h4><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" focusable="false" >
